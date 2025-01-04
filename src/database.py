c565bb1611df7e09a6e544ef93bdf026230e1cda
Kungusto <specifikbrateevo@gmail.com>
2025-01-04 19:03:15 +0300

    rooms

--- src/database.py
+++ src/database.py
@@ -1,9 +1,7 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy import text
 
 from sqlalchemy.orm import DeclarativeBase
 
-import asyncio
 
 from src.config import settings
 
@@ -16,4 +14,3 @@ async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 class Base(DeclarativeBase) : 
     pass
-
---
+++ src/migrations/versions/a5a377cdf166_init_rooms.py
@@ -0,0 +1,34 @@
+"""init rooms
+
+Revision ID: a5a377cdf166
+Revises: b3f69fe382a4
+Create Date: 2025-01-04 18:55:19.725258
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = 'a5a377cdf166'
+down_revision: Union[str, None] = 'b3f69fe382a4'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table('rooms',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('hotel_id', sa.Integer(), nullable=False),
+    sa.Column('title', sa.String(), nullable=False),
+    sa.Column('description', sa.String(), nullable=True),
+    sa.Column('price', sa.Integer(), nullable=False),
+    sa.Column('quantity', sa.Integer(), nullable=False),
+    sa.ForeignKeyConstraint(['hotel_id'], ['hotels.id'], ),
+    sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('rooms')
