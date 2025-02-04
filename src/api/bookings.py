6872ccfdeb5aa670e769f54352070a4721cac859
Kungusto <specifikbrateevo@gmail.com>
2025-02-04 21:46:05 +0300

    добавление ручек на получение отелей

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -11,9 +11,19 @@ async def create_booking(data: AddBookingsFromUser, db: DBDep, user_id: GetUserI
     time_of_booking = data.date_to - data.date_from
     price_for_one_day = await db.rooms.get_one_or_none(id=data.room_id)
     price = time_of_booking.days*price_for_one_day.price
-    bookings_data = data.dict()
-    bookings_data['user_id'], bookings_data['price'] = user_id, price
-    booking_add = AddBookings(**bookings_data)
+    booking_add = AddBookings(
+        user_id=user_id, 
+        price=price,
+        **data.dict())
     bookings_returned = await db.bookings.add(data=booking_add)
     await db.commit()
     return bookings_returned
+
+@router.get('/me')
+async def get_booking(user_id: GetUserId, db: DBDep) :
+    return await db.bookings.get_all(user_id=user_id)
+
+@router.get('')
+async def get_booking(db: DBDep) :
+    return await db.bookings.get_all()
+
