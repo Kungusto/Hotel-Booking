143ea853818cdf3f66e928a5dce76462ac451bc1
Kungusto <specifikbrateevo@gmail.com>
2025-02-10 09:44:35 +0300

    коммит

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -1,6 +1,9 @@
 from datetime import date
 from sqlalchemy import select, insert, func, update, delete
 
+# серсисы
+from src.repositories.utils import rooms_ids_for_booking
+
 ## Pydantic
 from repositories.utils import rooms_ids_for_booking
 from src.schemas.hotels import Hotel
@@ -17,7 +20,6 @@ class HotelsRepository(BaseRepository) :
     model = HotelsOrm 
     schema = Hotel
 
-# ---------------------------------------------------- #
     async def get_filtered_by_time(
         self, 
         date_from: date,
@@ -27,23 +29,31 @@ class HotelsRepository(BaseRepository) :
         limit: int = 0, 
         offset: int = 5
     ) :
-        filters_by = {}
-        
-        if location : 
-            filters_by['location'] = location
-        if title : 
-            filters_by['title'] = title
-        
-        rooms_stmt = rooms_ids_for_booking(
-            date_from=date_from,
-            date_to=date_to)
-        
-        hotels_id_to_get = (
+
+        rooms_stmt = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
+
+        hotels_ids_to_get = (
             select(RoomsOrm.hotel_id)
             .select_from(RoomsOrm)
             .filter(RoomsOrm.id.in_(rooms_stmt))
         )
-        return (await self.get_filtered(HotelsOrm.id.in_(hotels_id_to_get),
-                **filters_by
-            ))[offset:offset+limit]
-# ---------------------------------------------------- #
+
+        query = (
+            select(HotelsOrm)
+            .filter(HotelsOrm.id.in_(hotels_ids_to_get))
+        )
+
+        if title :
+            query = query.filter(func.lower(HotelsOrm.title).contains(title.strip().lower())) 
+        if location :
+            query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
+            
+        query = (
+                query
+                .limit(limit)
+                .offset(offset)
+        )
+                
+        result = await self.session.execute(query)
+                
+        return  [self.schema.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
