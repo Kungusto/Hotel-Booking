4e661b2185ca8ae5a89fc1b5465b6867fe5aa6db
Kungusto <specifikbrateevo@gmail.com>
2025-02-24 14:14:11 +0300

    редакт одного теста

--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -1,12 +1,13 @@
 from schemas.hotels import HotelAdd
-from src.api.dependencies import get_db
+from src.utils.dbmanager import DBManager
+from src.database import async_session_maker_null_pool
 
 async def test_add_hotel() : 
     data = HotelAdd(
         title="five stars",
         location="Москва"
     )
-    async for db in get_db() :
+    async with DBManager(session_factory=async_session_maker_null_pool) as db :
         new_hotel_data = await db.hotels.add(data)
         await db.commit()
 
