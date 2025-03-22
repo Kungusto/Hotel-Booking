9016f164422babfdd0a428f7121363f4d394456f
Kungusto <specifikbrateevo@gmail.com>
2025-03-23 00:33:34 +0300

    незначительные изменения к фикстурах bookings/test_api

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,7 +1,6 @@
 from datetime import date
 from src.schemas.bookings import AddBookingsFromUser
-from src.database import async_session_maker_null_pool
-from src.utils.dbmanager import DBManager
+from tests.conftest import get_db_null_pool
 import pytest
 
 @pytest.mark.parametrize("room_id, date_from, date_to, status_code",[
@@ -31,11 +30,11 @@ async def test_add_booking(
         assert res["status"] == "OK"
         assert "data" in res
     
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 async def delete_bookings() :
-    async with DBManager(session_factory=async_session_maker_null_pool) as db :
-        await db.bookings.delete()
-        await db.commit()
+    async for _db in get_db_null_pool() :
+        await _db.bookings.delete()
+        await _db.commit()
 
 @pytest.mark.parametrize("room_id, date_from, date_to, num_of_booking", [
     (1, "2025-03-21", "2025-03-23", 1),
@@ -45,9 +44,10 @@ async def delete_bookings() :
     (1, "2025-11-21", "2026-01-01", 5),
     (1, "2025-02-21", "2025-03-03", 6),
 ])
-async def test_add_and_get_my_bookings(delete_bookings, 
+async def test_add_and_get_my_bookings(
         authenticated_ac,
-        room_id, date_from, date_to, num_of_booking
+        room_id, date_from, date_to, num_of_booking,
+        delete_bookings
         ) :  
     response_post = await authenticated_ac.post(
         url="/bookings/create_booking",
@@ -57,8 +57,11 @@ async def test_add_and_get_my_bookings(delete_bookings,
             "date_to" : date_to
           }    
     )
+    assert response_post.status_code == 200
     response_get = await authenticated_ac.get(
         url="/bookings/me"
     )
     bookings_now = response_get.json()
+    assert response_get.status_code == 200
+    assert isinstance(bookings_now, list)
     assert len(bookings_now) == num_of_booking
