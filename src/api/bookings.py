76c15bcee9d4ca3ee04a979f708342a7efe5c572
Kungusto <specifikbrateevo@gmail.com>
2025-03-23 11:03:46 +0300

    значительна облегчена для чтения ручка на добавление бронирований

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -8,21 +8,16 @@ router = APIRouter(prefix='/bookings', tags=['Бронирование'])
 
 @router.post('/create_booking')
 async def create_booking(data: AddBookingsFromUser, db: DBDep, user_id: GetUserId) : 
-    time_of_booking = data.date_to - data.date_from
+    # Вычисляем цену
     price_for_one_day = await db.rooms.get_one_or_none(id=data.room_id)
-    price = time_of_booking.days*price_for_one_day.price
-
-    booking_add = AddBookings(
-        user_id=user_id, 
-        price=price,
-        **AddBookingsFromUser.model_dump(data))
-    # достаем номера, которые можно забронировать на этот интервал
-    hotel_id = (await db.rooms.get_one_or_none(id=data.room_id)).hotel_id
-    available_rooms : list[int] = await db.bookings.get_available_room(hotel_id, date_from=data.date_from, date_to=data.date_to)
-    # проверяем, есть ли указанный пользователем номер в списке свободных
-    if not (data.room_id in available_rooms) :
-        raise HTTPException(500)
-    bookings_returned = await db.bookings.add(data=booking_add)
+    price = ((data.date_to - data.date_from)).days*price_for_one_day.price
+    bookings_returned = await db.bookings.add_booking(
+        AddBookings(
+            **AddBookingsFromUser.model_dump(data),
+            user_id=user_id,
+            price=price
+        )
+    )
     await db.commit()
     return {"status":"OK", "data":bookings_returned}
 
--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -1,7 +1,10 @@
+from fastapi import HTTPException
 from sqlalchemy import select
 
+from src.schemas.bookings import AddBookings
 from src.repositories.mappers.mappers import BookingDataMapper
 from src.repositories.base import BaseRepository
+from src.repositories.rooms import RoomsRepository
 from src.models.bookings import BookingsOrm
 from src.repositories.utils import rooms_ids_for_booking
 
@@ -12,6 +15,7 @@ class BookingsRepository(BaseRepository) :
     mapper = BookingDataMapper
 
     async def get_bookings_with_today_checkin(self) : 
+        '''Достать номера с сегодняшним заселением'''
         query = (
             select(BookingsOrm)
             .filter(BookingsOrm.date_from == date.today())
@@ -29,3 +33,13 @@ class BookingsRepository(BaseRepository) :
         rooms = await self.session.execute(test_data)
         result = rooms.scalars().all()
         return result
+
+    async def add_booking(self, data: AddBookings, ) :
+        '''Добавляет бронирование с учетом уже имеющихся'''
+        # достаем номера, которые можно забронировать на этот интервал
+        hotel_id = (await RoomsRepository(self.session).get_one_or_none(id=data.room_id)).hotel_id
+        available_rooms : list[int] = await self.get_available_room(hotel_id, date_from=data.date_from, date_to=data.date_to)
+        # проверяем, есть ли указанный пользователем номер в списке свободных
+        if not (data.room_id in available_rooms) :
+            raise HTTPException(500)
+        bookings_returned = await self.add(data=data)
