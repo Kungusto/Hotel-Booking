91a2b9dabbfdec099b50b8c9c47b69950db09e9c
Kungusto <specifikbrateevo@gmail.com>
2025-03-23 14:09:07 +0300

    коммит перед форматтером ruff`ом

--- src/api/auth.py
+++ src/api/auth.py
@@ -13,18 +13,18 @@ router = APIRouter(prefix='/auth', tags=['Авторизация и аутент
 pwd_context = CryptContext(schemes=['bcrypt'],  deprecated='auto') 
 
 @router.post('/register')
-async def register_user(
-    data: UserRequestAdd,
-    db: DBDep
-) : 
+async def register_user(data: UserRequestAdd, db: DBDep) : 
     hashedpassword = AuthService().hash_password(data.password)
-    new_user_data = UserAdd(email=data.email, hashedpassword=hashedpassword, name=data.name, nickname=data.nickname)
+    new_user_data = UserAdd(
+        email=data.email, 
+        hashedpassword=hashedpassword, 
+        name=data.name, 
+        nickname=data.nickname)
     try :
         hotel = await db.users.add(new_user_data)
         await db.commit() 
     except IntegrityError  :
         raise HTTPException(status_code=400, detail='Пользователь с таким email уже существует!')
-    
     return {'status':'OK', 'data':hotel}
 
 @router.post('/login') 
@@ -34,28 +34,21 @@ async def login_user(
     db: DBDep
 ) :
     user = await db.users.get_user_with_hashed_password(email = data.email)
-
     if not user : 
         raise HTTPException(status_code=401, detail='Пользователя с таким email не существует')
     if not AuthService().verify_password(data.password, user.hashedpassword) :
         raise HTTPException(status_code=401, detail='Неверный пароль')
-    
     acces_token = AuthService().create_access_token({'user_id':user.id})
     response.set_cookie(key='access_token', value=acces_token)
-    
     return {'acces_token' : acces_token}
 
 @router.get('/me')
-async def get_me(
-    data: UserIdDep
-) :
+async def get_me(data: UserIdDep) :
     async with async_session_maker() as session : 
         user = await UsersRepository(session).get_one_or_none(id=data['user_id'])
         return user
 
 @router.post('/logout')
-async def logout_user(
-    response: Response
-) :
+async def logout_user(response: Response) :
     response.delete_cookie('access_token')
     return {'status':'OK'}
